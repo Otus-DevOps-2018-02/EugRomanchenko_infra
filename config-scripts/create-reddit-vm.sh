db6a09ad5df92d4af370ce03701018e4d207020b
Eugeny Romanchenko <eromanchenko@assist.ru>
2018-03-22 20:13:21 +0300

    Add command for create Firewall rule on GCP and startup scritp for start application

--- config-scripts/create-reddit-vm.sh
+++ config-scripts/create-reddit-vm.sh
@@ -6,3 +6,13 @@ gcloud compute instances create reddit-app\
   --machine-type=g1-small \
   --tags puma-server \
   --restart-on-failure \
+  --metadata-from-file startup-script=start-application.sh
+
+gcloud compute firewall-rules create default-puma-server\
+  --direction=INGRESS \
+  --priority=1000 \
+  --network=default \
+  --action=ALLOW \
+  --rules=tcp:9292 \
+  --source-ranges=0.0.0.0/0 \
+  --target-tags=puma-server
---
+++ config-scripts/start-application.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+cd /home/appuser/reddit
+puma -d
