002bef34b4b2d33452623d7a19d898408e93c724
Eugeny Romanchenko <eromanchenko@assist.ru>
2018-04-01 15:01:36 +0300

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,13 @@
+resource "google_compute_firewall" "firewall_ssh" {
+  name    = "default-allow-ssh"
+  network = "default"
+  description = "Allow SSH from anywhere"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  priority = "65534"
+}
